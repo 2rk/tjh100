ca868e9b674b8a3298629e25fbeabb28baa07af4
Nigel Rausch <nigelr@brisbanerails.com>
2012-01-27 19:49:00 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tjh100::Application.config.secret_token = 'ffd6337bba9ebdb3c882cfe3352b8aa13caf7fad7125ee82b2c359d70a3a8f7339e7fc06bb958ce01b22a61ba6e7dffc18af4026af533bab9a831a21f81fe18c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tjh100::Application.config.session_store :cookie_store, :key => '_tjh100_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tjh100::Application.config.session_store :active_record_store
