819ee1834cc1578e03f6e478c2db9a72d82056cd
Nigel Rausch <nigelr@brisbanerails.com>
2013-01-12 19:08:45 +1000

    TJH100-1 Configure email service updating username & pasword for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,8 @@ Tjh100::Application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
+      :user_name      => "app10885348@heroku.com",
+      :password       => "Your Password",
       :domain         => 'heroku.com'  }
 
 end
