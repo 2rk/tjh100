e1cd037742162e78ad082e4dfca0e27ee8a27bb3
Sean Kelly <sean@kiiii.com>
2013-01-19 13:36:19 +1000

    sortable column headings name and artist in song index

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,9 +1,11 @@
 class SongsController < ApplicationController
   before_filter :authenticate_user!
 
+  helper_method :sort_column, :sort_direction
+
   def index
     @search = Song.search(params[:search])
-    @songs = @search.page(params[:page]).per(20)
+    @songs = @search.page(params[:page]).order(sort_column + " " + sort_direction).per(20)
     @song_ids = current_user.selections.map &:song_id
   end
 
@@ -44,5 +46,15 @@ class SongsController < ApplicationController
     @song.destroy
 
     redirect_to songs_url
+  end
+
+  private
+
+  def sort_column
+    Song.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
