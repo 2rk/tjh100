e54a7b723894bbcc30c68e250c1cdb7a7daae9b7
Nigel Rausch <nigelr@brisbanerails.com>
2013-01-26 13:27:41 +1000

    changed the tweet back to 10

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -48,7 +48,7 @@ class Tweet < ActiveRecord::Base
   end
 
   def self.get_feed
-    Twitter.user_timeline("triplej", count: 30).each do |tweet|
+    Twitter.user_timeline("triplej", count: 10).each do |tweet|
       Rails.logger.info "#{tweet.id} '#{tweet.text}'"
       self.create(tweet_id: tweet.id, status: tweet.text) unless find_by_tweet_id(tweet.id)
     end
