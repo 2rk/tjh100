94decbef222bb5c5534a3ad94f98d8fcc81382da
Nigel Rausch <nigelr@brisbanerails.com>
2013-01-26 15:57:39 +1000

    another missing search

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -39,7 +39,7 @@ class Tweet < ActiveRecord::Base
   end
 
   def parse_song
-    start_quote = (status.index("– ‘") || status.index("– ‘") || status.index("- ‘") || status.index("- ‘")).to_i+3
+    start_quote = (status.index("– ‘") || status.index("– ‘") || status.index("- ‘") || status.index("- ‘") || status.index("- '")).to_i+3
 
     p "start_quote = #{start_quote}"
     end_quote = (status.rindex('’') || status.rindex("'")).to_i
